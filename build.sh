498ee89ffe1e7742f104eb0cb572951cfbc93d1f
Alexis Pojomovsky <apojomovsky@gmail.com>
2018-09-12 12:28:02 -0300

    Make sure to initialize submodules and to register to ssh-agent

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+# Ensures your SSH private key has been added to the ssh-agent, adds it otherwise
+ssh-add /home/$USER/.ssh/id_rsa
+
+# Makes sure that submodules have been initialized and are up to date
+git submodule update --init --recursive
+
+# Builds docker image
 docker build -t rosindex/rosindex .
